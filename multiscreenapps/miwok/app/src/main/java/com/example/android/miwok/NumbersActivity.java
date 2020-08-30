b04dedbb3594931b71771a3de1ca2255f979279a
Shashi <shashi.coding@gmail.com>
2020-08-30 18:41:32 +0530

    miwok app code update

--- multiscreenapps/miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ multiscreenapps/miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,47 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Create an array of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Find the root view so we can add child views to it
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        // Create a variable to keep track of the current index position
+        int index = 0;
+
+        // Keep looping until we've reached the end of the list (which means keep looping
+        // as long as the current index position is less than the length of the list)
+        while (index < words.size()) {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.setText(words.get(index));
+
+            // Add this TextView as another child to the root view of this layout
+            rootView.addView(wordView);
+
+            // Increment the index variable by 1
+            index++;
+        }
+
+//        // increments by 1 each time the code in the loop is executed.
+//        for (int index = 0; index < words.size(); index++) {
+//            // Create a new TextView
+//            TextView wordView = new TextView(this);
+//            rootView.addView(wordView);
+//        }
     }
 }
